bbf43dd4bc4c9b03f7693f22cabb08ebb30d35b1
Pedro <pedrobargiela@gmail.com>
2025-07-28 12:38:14 +0200

    404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,19 +8,19 @@ export default function NotFound() {
       <div className="text-center">
         <p className="text-4xl font-bold text-primary sm:text-6xl">404</p>
         <h1 className="mt-4 font-headline text-3xl font-bold tracking-tight text-foreground sm:text-5xl">
-          Page not found
+        Página No Encontrada
         </h1>
         <p className="mt-6 text-base leading-7 text-muted-foreground">
-          Sorry, we couldn’t find the page you’re looking for.
+          Parece que te has perdido en el código. La página que buscas no existe o ha sido movida a otro universo digital.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <Button asChild>
             <Link href="/">
-              <ArrowLeft className="mr-2 h-4 w-4" /> Go back home
+              <ArrowLeft className="mr-2 h-4 w-4" /> Volver al portfolio
             </Link>
           </Button>
           <Button variant="link" asChild>
-            <Link href="/contact">Contact support</Link>
+            <Link href="mailto:pedrobargiela@gmail.com">Contáctame por alguna duda</Link>
           </Button>
         </div>
       </div>
